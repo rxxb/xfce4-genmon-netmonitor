22378698b7d97bbd292d30bacddf26f11cb35999
rxxb <71070078+rxxb@users.noreply.github.com>
2020-10-11 18:53:31 +0700

    Update netmon.sh

--- netmon.sh
+++ netmon.sh
@@ -17,7 +17,7 @@ else
 	tx_prev=`cat ~/.cache/tx_prev_bytes`
 	rx_comp=$((($rx_curr-$rx_prev)*4))
 	tx_comp=$((($tx_curr-$tx_prev)*4))
-	numfmt --to=iec-i --suffix=B --field=2,4 --format='%.2f' <<< "<txt>Incoming: $rx_comp Outcoming: $tx_comp </txt>"
+	numfmt --to=iec-i --suffix=B --field=2,4 --format='%.2f' <<< "<txt>Incoming: $rx_comp Outgoing: $tx_comp </txt>"
 	echo $rx_curr > ~/.cache/rx_prev_bytes
 	echo $tx_curr > ~/.cache/tx_prev_bytes
 fi
