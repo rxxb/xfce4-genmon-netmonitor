077594405eef1aec70be2ba12c19d3c18f45b41b
rxxb <71070078+rxxb@users.noreply.github.com>
2020-10-11 18:53:04 +0700

    Update netmon.sh

--- netmon.sh
+++ netmon.sh
@@ -1,33 +1,23 @@
 #!/bin/bash
-
-#get interface name
-interface=`/usr/sbin/route | grep 'default' | sed 's/default.*\s//g'`
-
-#check interface
+interface=`ip r | grep '^default' | awk '{print $5}'`
 if [ -z $interface ]; then
-
-#display Offline if there's no interface
 	echo "<txt>Offline</txt>"
-elif [ ! -f ~/.cache/rx_prev_bytes ]; then
-
-#create "usage" cache
-	echo 0 > ~/.cache/rx_prev_bytes
-	echo 0 > ~/.cache/tx_prev_bytes
+	if [ -f ~/.cache/rx_prev_bytes ]; then
+		rm ~/.cache/rx_prev_bytes
+		rm ~/.cache/tx_prev_bytes
+	fi
 else
-
-	rx_prev=`cat ~/.cache/rx_prev_bytes`
-  tx_prev=`cat ~/.cache/tx_prev_bytes`
 	rx_curr=`cat /sys/class/net/$interface/statistics/rx_bytes`
-  tx_curr=`cat /sys/class/net/$interface/statistics/tx_bytes`
-  
-  #distract current usage with previous usage
-	rx_comp=$(($rx_curr-$rx_prev-73))
-	tx_comp=$(($tx_curr-$tx_prev-131))
-  
-  #formatting
+	tx_curr=`cat /sys/class/net/$interface/statistics/tx_bytes`
+	if [ ! -f ~/.cache/rx_prev_bytes ]; then
+		echo $rx_curr > ~/.cache/rx_prev_bytes
+		echo $tx_curr > ~/.cache/tx_prev_bytes
+	fi
+	rx_prev=`cat ~/.cache/rx_prev_bytes`
+	tx_prev=`cat ~/.cache/tx_prev_bytes`
+	rx_comp=$((($rx_curr-$rx_prev)*4))
+	tx_comp=$((($tx_curr-$tx_prev)*4))
 	numfmt --to=iec-i --suffix=B --field=2,4 --format='%.2f' <<< "<txt>Incoming: $rx_comp Outcoming: $tx_comp </txt>"
-  
-  #cache-ing for next
 	echo $rx_curr > ~/.cache/rx_prev_bytes
 	echo $tx_curr > ~/.cache/tx_prev_bytes
 fi
