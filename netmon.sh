e1e5fc2afd441e830c6803b0f453b7bababe6f86
rxxb <71070078+rxxb@users.noreply.github.com>
2020-10-20 09:37:45 +0700

    Update netmon.sh
    Fix, add vnstat tooltips

--- netmon.sh
+++ netmon.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 interface=`ip r | grep '^default' | awk '{print $5}'`
-if [ -z $interface ]; then
-	echo "<txt>Offline</txt>"
-	if [ -f ~/.cache/rx_prev_bytes ]; then
-		rm ~/.cache/rx_prev_bytes
-		rm ~/.cache/tx_prev_bytes
-	fi
-else
+vnstat=`which vnstat`
+if [ ! -z $interface ]; then
 	rx_curr=`cat /sys/class/net/$interface/statistics/rx_bytes`
 	tx_curr=`cat /sys/class/net/$interface/statistics/tx_bytes`
 	if [ ! -f ~/.cache/rx_prev_bytes ]; then
@@ -15,9 +10,30 @@ else
 	fi
 	rx_prev=`cat ~/.cache/rx_prev_bytes`
 	tx_prev=`cat ~/.cache/tx_prev_bytes`
-	rx_comp=$((($rx_curr-$rx_prev)*4))
-	tx_comp=$((($tx_curr-$tx_prev)*4))
+	rx_comp=$((($rx_curr-$rx_prev)*2))
+	tx_comp=$((($tx_curr-$tx_prev)*2))
 	numfmt --to=iec-i --suffix=B --field=2,4 --format='%.2f' <<< "<txt>Incoming: $rx_comp Outgoing: $tx_comp </txt>"
 	echo $rx_curr > ~/.cache/rx_prev_bytes
 	echo $tx_curr > ~/.cache/tx_prev_bytes
+else
+	echo "<txt>Offline</txt>"
+	if [ -f ~/.cache/rx_prev_bytes ]; then
+		rm ~/.cache/rx_prev_bytes
+		rm ~/.cache/tx_prev_bytes
+	fi
+fi
+
+# vnstat
+if [ ! -z $vnstat ];then
+	if [ -z $interface ]; then
+		printf '<tool>'
+		vnstat
+		printf '</tool>'
+	else
+		printf '<tool>'
+		vnstat | sed -e "s|$interface|$interface <span weight='Bold' fgcolor='Blue'>[current interface]</span> |"
+		printf '</tool>'
+	fi
+else
+	echo '<tool>You can install vnstat to track total usage</tool>'
 fi
